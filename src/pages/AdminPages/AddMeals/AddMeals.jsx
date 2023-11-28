b6543ec7f8cac4ff46ec0e7c2772d2ed6c713d5c
Ricky Ponting <qarimahdi217@gmail.com>
2023-11-28 13:43:32 +0600

    Extract all the data from add to meals admin dashboard from using react hook form.

--- src/pages/AdminPages/AddMeals/AddMeals.jsx
+++ src/pages/AdminPages/AddMeals/AddMeals.jsx
@@ -1,47 +1,52 @@
-
+import { useForm } from "react-hook-form"
 
 const AddMeals = () => {
+    const {
+        register,
+        handleSubmit,
+        reset
+    } = useForm()
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const form = e.target;
+    const onSubmit = (data) => {
+        console.log(data);
+        reset();
     }
 
     return (
         <div className="border-2 md:w-[600px] lg:w-[800px] p-8 m-8 shadow-2xl">
-            <form onSubmit={handleSubmit} className="space-y-3">
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
                 {/* meal title */}
                 <div className="form-control w-full ">
                     <label className="label">
                         <span className="label-text">What is title of the meal?</span>
                     </label>
-                    <input type="text" placeholder="Meal Title" className="input input-bordered w-full" />
+                    <input type="text" {...register("mealTitle")} placeholder="Meal Title" className="input input-bordered w-full" />
                 </div>
                 {/* admin name */}
                 <div className="form-control w-full ">
                     <label className="label">
                         <span className="label-text">What is your name?</span>
                     </label>
-                    <input type="text" placeholder="Your Name" className="input input-bordered w-full" />
+                    <input type="text" {...register("adminName")} placeholder="Your Name" className="input input-bordered w-full" />
                 </div>
                 {/* admin email */}
                 <div className="form-control w-full ">
                     <label className="label">
                         <span className="label-text">What is your email?</span>
                     </label>
-                    <input type="text" placeholder="Your Email" className="input input-bordered w-full" />
+                    <input type="text" {...register("adminEamil")} placeholder="Your Email" className="input input-bordered w-full" />
                 </div>
                 {/* meal likes */}
                 <div className="form-control w-full ">
                     <label className="label">
                         <span className="label-text">Input the primary likes count for the meal?</span>
                     </label>
-                    <input type="number" placeholder="Meal Likes Count" className="input input-bordered w-full" />
+                    <input type="number" {...register("mealLikes")} placeholder="Meal Likes Count" className="input input-bordered w-full" />
                 </div>`
                 {/* upcoming or add to meals */}
                 <div>
                     <label htmlFor="meal-type">Choose where you want show this meal?</label>
-                    <select id="meal-type" className="select select-bordered w-full max-w-xs ml-4">
+                    <select id="meal-type" {...register("upcomingOrAddMeals")} className="select select-bordered w-full max-w-xs ml-4">
                         <option disabled selected>Upcoming meals</option>
                         <option>Add to meals</option>
                     </select>
@@ -49,7 +54,7 @@ const AddMeals = () => {
                 {/* meal type */}
                 <div>
                     <label htmlFor="meal-type">Choose the meal type?</label>
-                    <select id="meal-type" className="select select-bordered w-full max-w-xs ml-4">
+                    <select id="meal-type" {...register("mealType")} className="select select-bordered w-full max-w-xs ml-4">
                         <option disabled selected>Breakfast</option>
                         <option>Lunch</option>
                         <option>Dinner</option>
@@ -58,16 +63,16 @@ const AddMeals = () => {
                 {/* meal image */}
                 <div>
                     <label htmlFor="meal-img">Choose the meal img</label>
-                    <input type="file" id='meal-img' className="file-input file-input-bordered w-full max-w-xs ml-4" />
+                    <input type="file" {...register("mealImg")} id='meal-img' className="file-input file-input-bordered w-full max-w-xs ml-4" />
                 </div>
                 <div className="divider"></div>
                 {/* meal description */}
                 <div>
-                    <textarea className="textarea textarea-bordered" placeholder="White a description for the meal?"></textarea>
+                    <textarea {...register("mealDescription")} className="textarea textarea-bordered" placeholder="White a description for the meal?"></textarea>
                 </div>
                 {/* meal review */}
                 <div>
-                    <textarea className="textarea textarea-bordered" placeholder="Write a review for the meal?"></textarea>
+                    <textarea {...register("mealReview")} className="textarea textarea-bordered" placeholder="Write a review for the meal?"></textarea>
                 </div>
                 <input type="submit" value="Submit" className="btn btn-primary font-bold" />
             </form>
